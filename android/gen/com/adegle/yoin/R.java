de5de3c80b6caf9fc508a24a35816902baacf0f7
YuchengLan <yuchenglan.dev@gmail.com>
2012-01-17 23:40:31 +0800

    add listview

--- android/gen/com/adegle/yoin/R.java
+++ android/gen/com/adegle/yoin/R.java
@@ -13,8 +13,17 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int listView1=0x7f050005;
+        public static final int relativeLayout1=0x7f050000;
+        public static final int title=0x7f050002;
+        public static final int uploader=0x7f050003;
+        public static final int view=0x7f050004;
+        public static final int youtube_img=0x7f050001;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int list_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- android/src/com/adegle/yoin/YoinActivity.java
+++ android/src/com/adegle/yoin/YoinActivity.java
@@ -4,37 +4,83 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreProtocolPNames;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 
 public class YoinActivity extends Activity {
 	private String jsonString;
+
+	private ListView listView;
+	String page;
+	String str = "";
+	String url = "http://yoinadegle.appspot.com/get_videos?order=view";
+
+	// private static final String IMG_URL =
+	// "http://img.youtube.com/vi/fcmc807j304/2.jpg";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		listView = (ListView) findViewById(R.id.listView1);
+		SimpleAdapter adapter = new SimpleAdapter(this, getData(),
+				R.layout.list_item, new String[] { "youtube_img", "title",
+						"uploader", "view" }, new int[] { R.id.youtube_img,
+						R.id.title, R.id.uploader, R.id.view });
+		listView.setAdapter(adapter);
+
 		jsonString = getJSONFromURL("http://yoinadegle.appspot.com/get_videos?order=view");
 		Log.e("123", jsonString);
+	}
 
+	private ArrayList<HashMap<String, Object>> getData() {
+		ArrayList<HashMap<String, Object>> arrayList = new ArrayList<HashMap<String, Object>>();
+		for (int i = 1; i <= 10; i++) {
+			try {
+
+				HashMap<String, Object> map = new HashMap<String, Object>();
+				map.put("youtube_img", "");// 影片縮圖
+				map.put("title", "標題" + i);// 標題
+				map.put("uploader", "上傳者" + i);// 上傳者
+				map.put("view", "觀看人數" + i);// 觀看人數
+				arrayList.add(map);
+
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		return arrayList;
 	}
 
 	private String getJSONFromURL(String string) {
 		String responseString = "";
-		
 		try {
-
-			URL url = new URL("http://yoinadegle.appspot.com/get_videos?order=view");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			URL url = new URL(
+					"http://yoinadegle.appspot.com/get_videos?order=view");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					url.openStream()));
 			responseString = reader.readLine();
+
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+			Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT)
+					.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 			Toast.makeText(this, "IOException", Toast.LENGTH_SHORT).show();
