abd56e827412d3cf42812cd224626dad494d2d0d
Jason C. Huang <chaoju.huang@gmail.com>
2012-01-17 15:50:22 +0800

    add android project

---
+++ android/src/com/adegle/yoin/YoinActivity.java
@@ -0,0 +1,13 @@
+package com.adegle.yoin;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class YoinActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
