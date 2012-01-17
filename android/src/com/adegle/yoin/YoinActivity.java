b1137a4b4680f78c941d1032e14fcfd9e88fa91f
Jason C. Huang <chaoju.huang@gmail.com>
2012-01-17 19:05:51 +0800

    add getJSONFromURL method

--- android/src/com/adegle/yoin/YoinActivity.java
+++ android/src/com/adegle/yoin/YoinActivity.java
@@ -1,13 +1,46 @@
 package com.adegle.yoin;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class YoinActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private String jsonString;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		jsonString = getJSONFromURL("http://yoinadegle.appspot.com/get_videos?order=view");
+		Log.e("123", jsonString);
+
+	}
+
+	private String getJSONFromURL(String string) {
+		String responseString = "";
+		
+		try {
+
+			URL url = new URL("http://yoinadegle.appspot.com/get_videos?order=view");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			responseString = reader.readLine();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+			Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT).show();
+		} catch (IOException e) {
+			e.printStackTrace();
+			Toast.makeText(this, "IOException", Toast.LENGTH_SHORT).show();
+		}
+
+		return responseString;
+	}
+
 }
