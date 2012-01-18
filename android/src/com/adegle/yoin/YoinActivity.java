6606c0d8576cd9a73ecbc0383b3c1746ea933802
Jason C. Huang <chaoju.huang@gmail.com>
2012-01-18 13:39:59 +0800

    solve json fix #2

--- android/src/com/adegle/yoin/YoinActivity.java
+++ android/src/com/adegle/yoin/YoinActivity.java
@@ -18,6 +18,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -39,102 +42,58 @@ import com.parse.PushService;
 
 public class YoinActivity extends Activity {
 	private String jsonString;
+	ArrayList<HashMap<String, String>> arrayList = new ArrayList<HashMap<String, String>>();
 
 	private ListView listView;
-	private ImageView img01;
-	String page;
-	String str = "";
-	String url = "http://yoinadegle.appspot.com/get_videos?order=view";
-
-	// private static final String IMG_URL =
-	// "http://img.youtube.com/vi/fcmc807j304/2.jpg";
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		
+
 		Parse.initialize(this, "0wvSw382QmOSDfFfLY9cjEwzjdCD0BkdWn8bus6j", "dlOrxUpPqPVALHFkwq2ys5QdROp63SDAQim7BISU");
 		PushService.subscribe(this, "", YoinActivity.class, R.drawable.noticon);
+		
+		findViews();
+		setListeners();
 
-		listView = (ListView) findViewById(R.id.listView1);
-		img01 = (ImageView) findViewById(R.id.youtube_img);
-		SimpleAdapter adapter = new SimpleAdapter(this, getData(),
-				R.layout.list_item, new String[] { "youtube_img", "title",
-						"uploader", "view" }, new int[] { R.id.youtube_img,
-						R.id.title, R.id.uploader, R.id.view });
+	}
+
+	private void setListeners() {
+		SimpleAdapter adapter = new SimpleAdapter(this, getData(), R.layout.list_item, new String[] { "youtube_img", "title", "uploader", "view" }, new int[] { R.id.youtube_img, R.id.title, R.id.uploader, R.id.view });
 		listView.setAdapter(adapter);
 		listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> a, View v, int position,
-					long id) {
-				touch();
+			public void onItemClick(AdapterView<?> a, View v, int position, long id) {
+				Uri uri = Uri.parse("http://www.youtube.com/v/" + arrayList.get(position).get("vid"));
+				Intent it = new Intent(Intent.ACTION_VIEW, uri);
+				startActivity(it);
 			}
 		});
-		jsonString = getJSONFromURL("http://yoinadegle.appspot.com/get_videos?order=view");
-		Log.e("123", jsonString);
+	}
+
+	private void findViews() {
+		listView = (ListView) findViewById(R.id.listView1);
 	}
 
 	private ArrayList<HashMap<String, String>> getData() {
-		ArrayList<HashMap<String, String>> arrayList = new ArrayList<HashMap<String, String>>();
-		for (int i = 0; i <= 10; i++) 
-		{
-			HashMap<String, String> map = new HashMap<String, String>();
-			map.put("youtube_img", "");// 標題
-			map.put("title", "標題：");// 標題
-			map.put("uploader", "上傳者：");// 上傳者
-			map.put("view", "觀看人氣：");// 觀看人數
-			arrayList.add(map);
-//			try {
-////				URL url = new URL("http://img.youtube.com/vi/B0CdELwhab4/2.jpg");
-////				URLConnection conn = url.openConnection();
-////
-////				HttpURLConnection httpConn = (HttpURLConnection) conn;
-////				httpConn.setRequestMethod("GET");
-////				httpConn.connect();
-////
-////				if (httpConn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-////					InputStream inputStream = httpConn.getInputStream();
-////
-////					Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-////					inputStream.close();
-////					
-////					img01.setImageBitmap(bitmap);
-////				}
-//
-//				
-//			} catch (Exception e) {
-//				// TODO: handle exception
-//			}
-
+		jsonString = getJSONFromURL("http://yoinadegle.appspot.com/get_videos?order=view");
+		try {
+			JSONArray array = new JSONArray(jsonString);
+			for(int i=0; i<array.length(); i++){
+				HashMap<String, String> map = new HashMap<String, String>();
+				JSONObject obj = array.getJSONObject(i);
+				map.put("youtube_img", "xxx");// 標題
+				map.put("title", obj.getString("title"));// 標題
+				map.put("uploader", obj.getString("uploader"));// 上傳者
+				map.put("view", obj.getString("view"));// 觀看人數
+				map.put("vid", obj.getString("vid"));
+				arrayList.add(map);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		// try
-		// {
-		// StringBuffer sb= new StringBuffer();
-		// String url = "http://yoinadegle.appspot.com/get_videos?order=view";
-		// String body = getContent(url);
-		// JSONArray array = new JSONArray(body);
-		// for(int i = 0;i<=array.length();i++)
-		// {
-		// JSONObject obj = array.getJSONObject(i);
-		// HashMap<String, Object> map = new HashMap<String, Object>();
-		// map.put("title", "標題" + sb.append(obj.getString("good")));// 標題
-		// map.put("uploader", "上傳者" + sb.append(obj.getString("good")));// 上傳者
-		// map.put("view", "觀看人數" + sb.append(obj.getString("good")));// 觀看人數
-		// arrayList.add(map);
-		// }
-		//
-		// } catch (Exception e) {
-		// // TODO: handle exception
-		// }
 		return arrayList;
-	}
-
-	private void touch() {
-
-		Uri uri = Uri.parse("http://www.youtube.com/v/dBiBsBY98uE");
-		Intent it = new Intent(Intent.ACTION_VIEW, uri);
-		startActivity(it);
 	}
 
 	private String getContent(String url) throws Exception {
@@ -146,8 +105,7 @@ public class YoinActivity extends Activity {
 		HttpResponse response = client.execute(new HttpGet(url));
 		HttpEntity entity = response.getEntity();
 		if (entity != null) {
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					entity.getContent()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(entity.getContent()));
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				sb.append(line + "\n");
@@ -160,16 +118,13 @@ public class YoinActivity extends Activity {
 	private String getJSONFromURL(String string) {
 		String responseString = "";
 		try {
-			URL url = new URL(
-					"http://yoinadegle.appspot.com/get_videos?order=view");
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					url.openStream()));
+			URL url = new URL("http://yoinadegle.appspot.com/get_videos?order=view");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 			responseString = reader.readLine();
 
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
-			Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT)
-					.show();
+			Toast.makeText(this, "MalformedURLException", Toast.LENGTH_SHORT).show();
 		} catch (IOException e) {
 			e.printStackTrace();
 			Toast.makeText(this, "IOException", Toast.LENGTH_SHORT).show();
